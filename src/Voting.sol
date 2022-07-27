f8aad183e52d761724cae6d2412f2d6337780ca6
Caranell <lyachek.anton@gmail.com>
2022-07-27 15:33:04 +0400

    Remove unused comments

--- src/Voting.sol
+++ src/Voting.sol
@@ -3,25 +3,14 @@ pragma solidity ^0.8.12;
 
 import "./VotingToken.sol";
 
-//  Implement some form of voting ballot**
-
-// Implement a pair of contracts: one that serves as a voting ballot and another as a token.
-// This voting ballot allows holders of the token to create proposals and to vote on said proposals.
-// The voting period duration must be identical for all proposals and should be defined on the voting ballot contract.
-// Proposals created on the voting ballot must include a predefined list of voting options.
-// Any token holder can vote on any ongoing proposal or delegate their voting power to another holder.
-// A proposal is only passed if a quorum, predefined in the voting ballot itself, is reached.
-
 // FIXME: check all uints and ints
 
-// TODO: To think about : if 80% votes for anotherOption, we can disable current proposal
 contract Ballot {
     address immutable ticketTokenAddr;
     uint256 immutable votingPeriod;
     address immutable owner;
     uint8 immutable quroumRequiredPercentage;
 
-    // reworked events (quorumReached + finished -> quorumReached+ProposalFinished+ProposalTImedOut)
     event ProposalQuorumReached(
         string indexed proposalName,
         string winningOption
@@ -38,7 +27,6 @@ contract Ballot {
     mapping(string => Proposal) public proposals;
 
     struct Proposal {
-        // ??? should we have name or mapping would suffice
         uint256 creationTime;
         // prevent computing 'creation+voting period' every time
         uint256 deadlineTime;
@@ -76,7 +64,6 @@ contract Ballot {
 
     modifier userHasTokens() {
         require(VotingTicket(ticketTokenAddr).balanceOf(msg.sender) != 0);
-        // TODO: depends on the implementation, maybe we should also check number of delegated votes
         _;
     }
 
@@ -89,8 +76,6 @@ contract Ballot {
 
         // checking that there're at least 2 options or more
         require(_votingOptions.length > 1);
-        // TODO: what should we do with proposals with identical names? maybe hash them
-        // what if same creator adds same proposal
 
         Proposal storage newProposal = proposals[_name];
         newProposal.creationTime = block.timestamp;
@@ -113,7 +98,7 @@ contract Ballot {
         isProposalTimeFinished(_name)
     {
         uint256 numberOfVotesAvailable = VotingTicket(ticketTokenAddr)
-            .numberOfVotes(msg.sender);
+            .numberOfVotesAvailable(msg.sender);
         require(numberOfVotesAvailable >= _votes);
 
         proposals[_name].optionVotes[_option] += numberOfVotesAvailable;
